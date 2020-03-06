4aed605259e199333af17e7854b5166c47043628
root <prashant.kgupta.met15@itbhu.ac.in>
2020-03-06 06:46:30 +0000

    Chocolate Problem

---
+++ cf617B.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for(int i=0; i<n; i++)
+	{
+		int x;
+		cin >> x;
+		v[i] = x;
+	}
+	int res = 1, x,y;
+	x = INT_MIN;
+	y = INT_MIN;
+	
+	for(int i=0; i<n; i++)
+	{
+		if(v[i] == 1)
+		{
+			x = y;
+			y = i;
+			if(x != INT_MIN && y != INT_MIN)
+			{
+				res *= (y-x);
+			}
+		}
+	}
+	
+	if(x == INT_MIN && y == INT_MIN)
+	{
+		res = 0;
+	}
+	cout << res;
+	
+	return 0;
+}
